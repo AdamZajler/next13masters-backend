9a63a437be0507877f1373fd1155a6c0de003eff
Adam Zajler <adamzajler01@gmail.com>
2023-10-12 15:00:54 +0200

    Added graphql to prod

--- config/plugins.js
+++ config/plugins.js
@@ -20,4 +20,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
 });
